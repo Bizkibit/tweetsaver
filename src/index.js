52eb9d592e2023c9677b4d77966eaf0cb427436d
Farshid Heshmati <F.Heshmati@celayix.com>
2020-07-08 13:02:04 -0700

    added a 2nd column

--- src/index.js
+++ src/index.js
@@ -1,47 +1,86 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
 import initialData from "./initial-data";
 import Column from "./column";
 import { DragDropContext } from "react-beautiful-dnd";
 
+const Container = styled.div`
+  display: flex;
+`;
+
 class App extends React.Component {
   state = initialData;
 
-  onDragEnd = (result) => {
+  //callback for handling dropping action
+  onDragEnd = result => {
     console.log(result);
-    const {destination, source, draggableId} = result;
+    debugger
+    const { destination, source, draggableId } = result;
 
     if (!destination) {
-      return
+      return;
     }
 
     if (
       destination.droppableId === source.droppableId &&
       destination.index === source.index
     ) {
-      return
+      return;
     }
 
-    const column = this.state.columns[source.droppableId];
-    const newTaskIds = Array.from(column.taskIds);
-    newTaskIds.splice(source.index, 1);
-    newTaskIds.splice(destination.index, 0, draggableId);
+    const start = this.state.columns[source.droppableId];
+    const finish = this.state.columns[destination.droppableId];
 
-    const newColumn = {
-      ...column,
-      taskIds: newTaskIds,
+    if (start === finish) {
+      const column = start;
+      const newTaskIds = Array.from(column.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+  
+      const newColumn = {
+        ...column,
+        taskIds: newTaskIds
+      };
+  
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]: newColumn
+        }
+      };
+  
+      this.setState(newState);
+    } else {
+      const startTaskIds = Array.from(start.taskIds);
+      startTaskIds.splice(source.index, 1);
+
+      const newStart = {
+        ...start,
+        taskIds: startTaskIds
+      };
+
+      const finishTaskIds = Array.from(finish.taskIds);
+      finishTaskIds.splice(destination.index, 0, draggableId);
+
+      const newFinish = {
+        ...finish,
+        taskIds: finishTaskIds
+      };
+
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newStart.id]: newStart,
+          [newFinish.id]: newFinish
+        }
+      };
+      this.setState(newState);
     }
 
-    const newState = {
-      ...this.state,
-      columns: {
-        ...this.state.columns,
-        [newColumn.id]: newColumn,
-      }
-    }
-
-    this.setState(newState);
-  }
+  };
 
   render() {
     return (
@@ -50,17 +89,17 @@ class App extends React.Component {
         // onDragUpdate
         //required
         onDragEnd={this.onDragEnd}
-      
       >
-        {this.state.columnOrder.map(columnId => {
-          const { [columnId]: column } = this.state.columns;
-          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
-          console.log(column);
-          console.log(tasks);
-
-          // return column.title;
-          return <Column key={column.id} column={column} tasks={tasks} />;
-        })}
+        <Container>
+          {this.state.columnOrder.map(columnId => {
+            const { [columnId]: column } = this.state.columns;
+            const tasks = column.taskIds.map(
+              taskId => this.state.tasks[taskId]
+            );
+            // return column.title;
+            return <Column key={column.id} column={column} tasks={tasks} />;
+          })}
+        </Container>
       </DragDropContext>
     );
   }
--- src/initial-data.js
+++ src/initial-data.js
@@ -13,11 +13,18 @@ const initialData = {
             //indicated owndership
             //maintain order
             taskIds: ['task-1', 'task-2', 'task-3', 'task-4', 'task-5']
+        },
+        'column-2': {
+            id: 'column-2',
+            title: 'In progress',
+            //indicated owndership
+            //maintain order
+            taskIds: []
         }
     },
 
     //order of columns
-    columnOrder : ['column-1']
+    columnOrder : ['column-1', 'column-2']
 }
 
 export default initialData;

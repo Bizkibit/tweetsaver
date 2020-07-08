925019b021792af962bc729cd530aef15eb1b17f
Farshid Heshmati <F.Heshmati@celayix.com>
2020-07-08 11:32:50 -0700

    added column and task components

--- src/column.js
+++ src/column.js
@@ -1,7 +1,32 @@
 import React from "react";
+import styled from "styled-components";
+import Task from './task';
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+`;
+const Title = styled.h3`
+  padding: 8px;
+`;
+const TaskList = styled.div`
+  padding: 8px;
+`;
 
 export default class Column extends React.Component {
-    render() {
-        return this.props.column.title
-    }
+  render() {
+    // return this.props.column.title
+    return (
+      <Container>
+        {/* this is where the searchbar component will go */}
+        <Title>{this.props.column.title}</Title>
+        <TaskList>
+          {this.props.tasks.map(task => (
+            <Task key={task.id} task={task} />
+          ))}
+        </TaskList>
+      </Container>
+    );
+  }
 }

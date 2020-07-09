428a16370a941d00fc6e72926a2420112d349b36
Farshid Heshmati <F.Heshmati@celayix.com>
2020-07-09 02:28:52 -0700

    added util func for tweet obj

--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,5 @@
+import { prepareTweetsObject } from './utils/prepareTweetsObject'
+
 export const searchTweet = query =>
   fetch("/api/search", {
     method: "POST",
@@ -9,29 +11,7 @@ export const searchTweet = query =>
     .then(res => res.json())
     .then(({ data = {} }) => {
       const { statuses = [] } = data;
-      const tweets = {};
-      statuses
-        .map(status => {
-          const {
-            id_str: id,
-            text: content,
-            created_at: date,
-            user: {
-              name,
-              profile_image_url_https: pic,
-              screen_name: handle
-            } = {}
-          } = status;
-          return {
-            id,
-            content,
-            name,
-            pic,
-            handle,
-            date
-          };
-        })
-        .forEach(status => (tweets[status.id] = status));
+      const tweets = prepareTweetsObject(statuses);
       return tweets;
     })
     .catch(console.error);
@@ -46,29 +26,7 @@ export const searchTweet = query =>
   })
     .then(res => res.json())
     .then(({ data = [] }) => {
-      const tweets = {};
-      data
-      .map(status => {
-        const {
-          id_str: id,
-          text: content,
-          created_at: date,
-          user: {
-            name,
-            profile_image_url_https: pic,
-            screen_name: handle
-          } = {}
-        } = status;
-        return {
-          id,
-          content,
-          name,
-          pic,
-          handle,
-          date
-        };
-      })
-      .forEach(status => (tweets[status.id] = status));
+      const tweets = prepareTweetsObject(data);
       return tweets;
     })
     .catch(console.error);

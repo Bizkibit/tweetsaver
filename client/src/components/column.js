2313c96745066d038e32f0aa919c50ea04d56398
Farshid Heshmati <F.Heshmati@celayix.com>
2020-07-09 01:23:31 -0700

    added styled components

--- client/src/components/column.js
+++ client/src/components/column.js
@@ -1,46 +1,7 @@
 import React from "react";
-import styled from "styled-components";
 import { Droppable } from "react-beautiful-dnd";
 import Task from "./task";
-
-const Container = styled.div`
-  margin: 8px;
-  border: 1px solid lightgrey;
-  border-radius: 2px;
-  width: 400px;
-  display: flex;
-  flex-direction: column;
-`;
-const Title = styled.h3`
-  padding: 8px;
-  text-align: center;
-`;
-const TaskList = styled.div`
-  padding: 8px;
-  flex-grow: 1;
-  min-height: 100px;
-`;
-
-const SearchBar = styled.div`
-    display: flex;
-    height: 35px;
-    padding-top: 20px;
-    padding-bottom: 20px;
-    padding-left: 15px;
-    padding-right: 15px;
-`;
-
-const Btn = styled.button`
-  display: flex;
-  justify-content: center;
-  width: 35px;
-  height: 35px;
-  align-items: center;
-`;
-
-const Input =  styled.input`
-  width: 375px
-`;
+import { Container, Title, TaskList, SearchBar, Btn, Input } from './styledComponents';
 
 
 export default class Column extends React.Component {
